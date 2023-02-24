9c57117f55e56890097f9daf2ec6a766f52110f8
Katarzyna Szejko <katarzynaszejko@gmail.com>
2023-02-24 22:40:25 +0100

    dodanie statusow, licznika czasu, punktow

--- src/app/second/second.component.ts
+++ src/app/second/second.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter} from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-second',
@@ -12,11 +12,55 @@ export class SecondComponent {
     this.userName = this.data;
   }
  
-
   @Output() public onExit = new EventEmitter<boolean>();
 
   public onExitClick() {
     this.onExit.emit(true);
   }
+  public score = 0;
+  public seconds = 0;
+  public gameStatus = 'READY';
+  public started = false;
+  intervalId: any;
+
+  public pointsCount() {
+    this.score++;
+    console.log(this.score);
   }
+
+  public resetScoreAndTime() {
+    this.score = 0;
+    this.seconds = 0;
+    clearInterval(this.intervalId);
+  }
+
+  public stopTime() {
+    clearInterval(this.intervalId);
+  }
+  
+  public secondsCount() {
+    this.intervalId = setInterval(() => {
+      this.seconds++;
+    }, 1000);
+  }
+
+  public startGame() {
+    this.gameStatus = 'STARTED';
+    this.secondsCount();
+  }
+  public pauseGame() {
+    this.gameStatus = 'PAUSED';
+    this.stopTime();
+  }
+  public notStarted(): void {
+    this.gameStatus = 'READY';
+    this.resetScoreAndTime();
+  }
+  public gameOverStatus() {
+    this.gameStatus = 'GAME OVER';
+    this.stopTime();
+    this.resetScoreAndTime();
+  }
+}
+
 
